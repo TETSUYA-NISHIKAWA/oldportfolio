b6340fbd75d49d2f06acf3f55b3bafe522fd576e
TETSUYA-NISHIKAWA <89851789+TETSUYA-NISHIKAWA@users.noreply.github.com>
2022-03-18 16:43:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 $(function () {
 
+  //カーソル要素の指定
+  var cursor=$("#cursor");
+  //ちょっと遅れてついてくるストーカー要素の指定  
+  var stalker=$("#stalker");
+  
+  //mousemoveイベントでカーソル要素を移動させる
+  $(document).on("mousemove",function(e){
+    //カーソルの座標位置を取得
+    var x=e.clientX;
+    var y=e.clientY;
+    //カーソル要素のcssを書き換える用
+    cursor.css({
+      "opacity":"0.9",
+      "top":y+"px",
+      "left":x+"px"
+    });
+    //ストーカー要素のcssを書き換える用    
+    setTimeout(function(){
+      stalker.css({
+        "opacity":"0.3",
+        "top":y+"px",
+        "left":x+"px"
+      });
+    },40);//カーソルより遅れる時間を指定
+  });
+
   //ページ内スクロール
   var navHeight = $(".header").outerHeight();
 
